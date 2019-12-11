f32d813ebb762c7fa1375833ca92a14dba79a810
AleksandrZhuravel <cah40c@tut.by>
2019-12-11 10:25:14 +0300

    README.md supplemented, tests supplemented by @DisplayName, Thread.sleep deleted, negativeRequestNoPhone changed

--- src/test/java/TestFormField.java
+++ src/test/java/TestFormField.java
@@ -1,4 +1,5 @@
 import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
@@ -8,7 +9,8 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class TestFormField {
-    @Test
+   @Test
+   @DisplayName("Should show confirmation message if all fields completed correctly")
     public void positiveRequest() {
         open("http://localhost:9999");
         SelenideElement form = $(By.className("App_appContainer__3jRx1"));
@@ -18,14 +20,11 @@ public class TestFormField {
         form.$(By.className("button__content")).click();
         $("[data-test-id = order-success]").shouldHave(exactText("Ваша заявка успешно отправлена!" +
                 " Наш менеджер свяжется с вами в ближайшее время."));
-        try {
-            Thread.sleep(50000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+
     }
 
     @Test
+    @DisplayName("Should show error message if name-field completed incorrectly(Latin letters)")
     public void negativeRequestLatinName() {
         open("http://localhost:9999");
         SelenideElement form = $(By.className("App_appContainer__3jRx1"));
@@ -35,13 +34,10 @@ public class TestFormField {
         form.$(By.className("button__content")).click();
         $(By.className("input__sub")).shouldHave(exactText("Имя и Фамилия указаные неверно." +
                 " Допустимы только русские буквы, пробелы и дефисы."));
-        try {
-            Thread.sleep(50000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+
     }
         @Test
+        @DisplayName("Should show error message if name-field left empty")
         public void negativeRequestNoName () {
             open("http://localhost:9999");
             SelenideElement form = $(By.className("App_appContainer__3jRx1"));
@@ -49,13 +45,10 @@ public class TestFormField {
             form.$(By.className("checkbox__box")).click();
             form.$(By.className("button__content")).click();
             $(By.className("input__sub")).shouldHave(exactText("Поле обязательно для заполнения"));
-            try {
-                Thread.sleep(50000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+
         }
         @Test
+        @DisplayName("Should show error message if phone-field completed incorrectly(letters)")
         public void negativeRequestLettersInPhone () {
             open("http://localhost:9999");
             SelenideElement form = $(By.className("App_appContainer__3jRx1"));
@@ -64,25 +57,18 @@ public class TestFormField {
             form.$(By.className("checkbox__box")).click();
             form.$(By.className("button__content")).click();
             $(By.xpath("//div/form/div[2]/span/span/span[3]")).shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
-            try {
-                Thread.sleep(50000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+
         }
         @Test
+        @DisplayName("Should show error message if phone-field left empty")
         public void negativeRequestNoPhone () {
             open("http://localhost:9999");
             SelenideElement form = $(By.className("App_appContainer__3jRx1"));
             form.$("[data-test-id = name] input").setValue("Иванов Иван");
             form.$(By.className("checkbox__box")).click();
             form.$(By.className("button__content")).click();
-            $(By.xpath("//div/form/div[2]/span/span/span[3]")).shouldHave(exactText("Поле обязательно для заполнения"));
-            try {
-                Thread.sleep(50000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            $(By.xpath("//div[2]//span[3]")).shouldHave(exactText("Поле обязательно для заполнения"));
+
         }
     }
 
